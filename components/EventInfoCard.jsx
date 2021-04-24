fb3c9fdc15da1674a5fcfc36d2c9364e8c7a48cc
Sandaru Jayasekara <sandarulakshitha1999@gmail.com>
2021-04-24 22:09:45 +0530

    Design the event info card of dashboard

---
+++ components/EventInfoCard.jsx
@@ -0,0 +1,83 @@
+import React, {useState} from "react";
+import {Card, Container, makeStyles, Typography} from "@material-ui/core";
+import "./EventInfoCard.css"
+import EventInfoText from "./EventInfoText";
+
+// Styles for Material UI components
+const useStyles = makeStyles({
+    title: {
+        fontSize: "1.6em",
+        color: "white",
+        fontWeight: "bold",
+        width: "100%",
+        paddingBlockStart: "8px",
+    },
+    days: {
+        fontWeight: "bold",
+    },
+    toGo: {
+        fontSize: "1.5em",
+        alignItems: "center",
+        fontWeight: "500",
+        marginBlockStart: "-8px"
+    },
+    infoText: {
+        marginBlockStart: "20px",
+        marginBlockEnd: "16px"
+    },
+    container: {
+        padding: 0
+    }
+});
+
+const EventInfoCard = () => {
+    const styles = useStyles();
+
+    // States
+    const [eventTitle, setEventTitle] = useState("ICAF 2021");
+    const [remainingDays, setRemainingDays] = useState(10);
+    const [date, setDate] = useState("28th June 2021");
+    const [location, setLocation] = useState("SLIIT Auditorium");
+    const [time, setTime] = useState("8:00 am - 6.00 pm");
+
+    return (
+        <Card variant="outlined">
+            <Container className={styles.container}>
+                <Typography
+                    variant="h1"
+                    align="center"
+                    classes={{h1: styles.title}}
+                    className="eventTitle"
+                >
+                    {eventTitle}
+                </Typography>
+                <div className="background">
+                </div>
+            </Container>
+            <Container className={styles.container}>
+                <Typography
+                    variant="h2"
+                    align="center"
+                    classes={{h2: styles.days}}
+                >
+                    {remainingDays}
+                </Typography>
+                <Typography
+                    variant="h2"
+                    align="center"
+                    color="textSecondary"
+                    classes={{h2: styles.toGo}}
+                >
+                    days to go
+                </Typography>
+            </Container>
+            <Container className={`${styles.container} ${styles.infoText}`}>
+                <EventInfoText label="Conference Date" data={date} />
+                <EventInfoText label="Location" data={location} />
+                <EventInfoText label="Time" data={time} />
+            </Container>
+        </Card>
+    )
+}
+
+export default EventInfoCard;
---
+++ components/EventInfoText.jsx
@@ -0,0 +1,41 @@
+import React from "react";
+import Grid from "@material-ui/core/Grid";
+import {makeStyles, Typography} from "@material-ui/core";
+
+const useStyles = makeStyles({
+    all: {
+        paddingBlock: "2px",
+        paddingInline: "16px",
+        fontWeight: "bold"
+    }
+})
+
+const EventInfoText = props => {
+    const styles = useStyles();
+
+    return (
+        <>
+            <Grid container>
+                <Grid item xs="6">
+                    <Typography
+                        align="left"
+                        className={styles.all}
+                    >
+                        {props.label}
+                    </Typography>
+                </Grid>
+                <Grid item xs="6">
+                    <Typography
+                        align="right"
+                        color="textSecondary"
+                        className={styles.all}
+                    >
+                        {props.data}
+                    </Typography>
+                </Grid>
+            </Grid>
+        </>
+    )
+}
+
+export default EventInfoText
