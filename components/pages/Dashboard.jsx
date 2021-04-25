69445087b1f9b7396147d7df1db9629284c25e06
Sandaru Jayasekara <sandarulakshitha1999@gmail.com>
2021-04-25 15:48:25 +0530

    Design the total value cards of dashboard

--- components/pages/Dashboard.jsx
+++ components/pages/Dashboard.jsx
@@ -1,11 +1,18 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Title from '../Title';
 import {Grid} from "@material-ui/core";
 import RegistrationInfoCard from "../RegistrationInfoCard"
 import EventInfoCard from "../EventInfoCard";
+import TotalValueCard from "../TotalValueCard";
 
 //Admin Dashboard Page
 const Dashboard = () => {
+    // States
+    const [pendingEdits, setPendingEdits] = useState(0);
+    const [totalAttendees, setTotalAttendees] = useState(0);
+    const [totalPresenters, setTotalPresenters] = useState(0);
+    const [totalResearchers, setTotalResearchers] = useState(0);
+
     return (
         <>
             <div>
@@ -23,6 +30,38 @@ const Dashboard = () => {
                 </Grid>
                 <Grid item md={4}>
                     <EventInfoCard />
+                </Grid>
+                <Grid item md={3}>
+                    <TotalValueCard
+                        title="Pending Edit Approvals"
+                        value={pendingEdits}
+                        styles="pendingEdits"
+                        path="/edits"
+                    />
+                </Grid>
+                <Grid item md={3}>
+                    <TotalValueCard
+                        title="Total Attendees"
+                        value={totalAttendees}
+                        styles="totalAttendees"
+                        path="/registrations"
+                    />
+                </Grid>
+                <Grid item md={3}>
+                    <TotalValueCard
+                        title="Total Presenters"
+                        value={totalPresenters}
+                        styles="totalPresenters"
+                        path="/registrations"
+                    />
+                </Grid>
+                <Grid item md={3}>
+                    <TotalValueCard
+                        title="Total Researchers"
+                        value={totalResearchers}
+                        styles="totalResearchers"
+                        path="/registrations"
+                    />
                 </Grid>
             </Grid>
         </>
